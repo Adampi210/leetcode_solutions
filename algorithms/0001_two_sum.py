d5af13bfb46804764d7682ab3a658aa52d9093af
Adampi210 <adampi210u@gmail.com>
2024-12-27 23:17:03 +0100

    finished 1

--- algorithms/0001_two_sum.py
+++ algorithms/0001_two_sum.py
@@ -2,7 +2,10 @@
 
 def twoSum(nums, target):
     if_sum = dict()
+    # Go over all numbers in the list
     for i, num in enumerate(nums):
+        # If the compliment of the current number is in the dictionary, return the indices
         if target - num in if_sum:
             return [if_sum[target - num], i]
+        # Otherwise, add the number to the dictionary
         if_sum[num] = i
